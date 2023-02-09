92636bc5d5049f4d243546cfed703c1df1de4a44
jailcomfranssa <jail_son_0@hotmail.com>
2023-02-09 18:47:15 -0300

    configurando Eureka-Client service Hotel

--- HotelService/src/main/java/com/jf/hotel/HotelServiceApplication.java
+++ HotelService/src/main/java/com/jf/hotel/HotelServiceApplication.java
@@ -2,7 +2,9 @@ package com.jf.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HotelServiceApplication {
 
