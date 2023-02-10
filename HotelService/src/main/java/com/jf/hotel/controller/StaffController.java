d3ab41ca7f4d00f4c88065b53dde4dbfff0460e7
jailcomfranssa <jail_son_0@hotmail.com>
2023-02-10 17:08:04 -0300

    add mais de uma rota no predicates de ApiGateway

---
+++ HotelService/src/main/java/com/jf/hotel/controller/StaffController.java
@@ -0,0 +1,21 @@
+package com.jf.hotel.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffs(){
+
+        List<String> list = Arrays.asList("Ram", "Shyam", "Seeta","Krishma");
+        return ResponseEntity.ok(list);
+    }
+}
